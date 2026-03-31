17320c54d455dd396e48ae5ff08dab35fdfe16e6
Sumit Adep <sumit.adep@dyulabs.com>
2026-03-31 07:59:18 +0530

    Redirect printf statements to uart1

--- application/Core/Src/main.c
+++ application/Core/Src/main.c
@@ -97,7 +97,7 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    HAL_UART_Transmit(&huart1, (uint8_t *)"Hello, World!\r\n", sizeof("Hello, World!\r\n") - 1, HAL_MAX_DELAY);
+    printf("Hello World\r\n");
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -209,6 +209,11 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+int _write(int file, char *ptr, int len)
+{
+  HAL_UART_Transmit(&huart1, (uint8_t *)ptr, len, HAL_MAX_DELAY);
+  return len;
+}
 
 /* USER CODE END 4 */
 
